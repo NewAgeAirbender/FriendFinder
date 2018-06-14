942fe744eb1ea412022fc7c25c7d1fdbf7ca69f4
Rylie Johnson <paintedflame@gmail.com>
2018-06-13 23:20:06 -0500

    adds all the things

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # FriendFinder
+
+Survey that compares the users answers to an array of previous user's scores and matches them up with the most similarly answering person.
---
+++ app/data/friends.js
@@ -0,0 +1,11 @@
+var friendArray = [
+	{
+		name: "Ahmed",
+		photo: "afhaque89@gmail.com",
+		scores: [],
+		//fill in values
+	}
+];
+
+// Note how we export the array. This makes it accessible to other files using require. 
+module.exports = friendArray; 
